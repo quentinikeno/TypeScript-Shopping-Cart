6e9a8021b536a550aa3db10c5f9a060addc469aa
quentinikeno <ikenoq@gmail.com>
2023-04-14 16:44:06 -1000

    Add a useEffect to dispatch fetchProducts thunk.

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,17 @@
+import { useEffect } from "react";
 import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Store from "./pages/Store";
 import Navbar from "./components/Navbar";
+import { RootState, useAppDispatch } from "./redux/store";
+import { useSelector } from "react-redux";
+import { fetchProducts } from "./redux/productsSlice";
 
 function App() {
+	const dispatch = useAppDispatch();
+	useEffect(() => {
+		dispatch(fetchProducts("https://fakestoreapi.com/products"));
+	});
 	return (
 		<div className="App ">
 			<Navbar />
