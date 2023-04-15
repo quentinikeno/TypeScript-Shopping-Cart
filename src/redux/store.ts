0921e4cd581362bf424b98e81a4ee7f3f12ae55a
quentinikeno <ikenoq@gmail.com>
2023-04-14 16:43:47 -1000

    Add reducer for the productsSlice.

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,10 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
+import productsReducer from "./productsSlice";
+import { TypedUseSelectorHook, useDispatch, useSelector } from "react-redux";
 
 export const store = configureStore({
-	reducer: {},
+	reducer: { products: productsReducer },
 });
 
-// Infer the `RootState` and `AppDispatch` types from the store itself
 export type RootState = ReturnType<typeof store.getState>;
-// Inferred type: {posts: PostsState, comments: CommentsState, users: UsersState}
-export type AppDispatch = typeof store.dispatch;
+export const useAppDispatch: () => typeof store.dispatch = useDispatch;
+export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
