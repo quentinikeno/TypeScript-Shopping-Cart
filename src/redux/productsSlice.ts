b615910fcfb886eb305c3ec18c69bcd75bb3eae4
Quentin Ikeno <ikenoq@gmail.com>
2023-04-15 09:40:33 -1000

    Remove console.log.

--- src/redux/productsSlice.ts
+++ src/redux/productsSlice.ts
@@ -31,7 +31,6 @@ export const fetchProducts = createAsyncThunk(
 	async (URL: string, { rejectWithValue }) => {
 		try {
 			const response = await axios.get(URL);
-			console.log(response.data);
 			return response.data;
 		} catch (error) {
 			if (axios.isAxiosError(error)) {
