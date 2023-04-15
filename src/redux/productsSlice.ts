8b366f551590394f3a0192e3dd8505cfb33d0051
quentinikeno <ikenoq@gmail.com>
2023-04-14 16:43:13 -1000

    Add a thunk to fetch products from api.

--- src/redux/productsSlice.ts
+++ src/redux/productsSlice.ts
@@ -1,5 +1,5 @@
-import { createSlice } from "@reduxjs/toolkit";
-import type { PayloadAction } from "@reduxjs/toolkit";
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import axios from "axios";
 
 interface Product {
 	id: number;
@@ -16,16 +16,51 @@ interface Product {
 
 export interface ProductsState {
 	products: Product[];
+	isLoading: boolean;
+	error: string;
 }
 
 const initialState: ProductsState = {
 	products: [],
+	isLoading: false,
+	error: "",
 };
+
+export const fetchProducts = createAsyncThunk(
+	"products/fetch",
+	async (URL: string, { rejectWithValue }) => {
+		try {
+			const response = await axios.get(URL);
+			console.log(response.data);
+			return response.data;
+		} catch (error) {
+			if (axios.isAxiosError(error)) {
+				rejectWithValue(error.message);
+			}
+		}
+	}
+);
 
 export const productsSlice = createSlice({
 	name: "products",
 	initialState,
 	reducers: {},
+	extraReducers: (builder) => {
+		builder.addCase(fetchProducts.fulfilled, (state, action) => {
+			state.products = action.payload;
+			state.isLoading = false;
+			state.error = "";
+		});
+		builder.addCase(fetchProducts.pending, (state, action) => {
+			state.isLoading = true;
+		});
+		builder.addCase(fetchProducts.rejected, (state, action) => {
+			if (typeof action.payload === "string") {
+				state.error = action.payload;
+			}
+			state.isLoading = false;
+		});
+	},
 });
 
 export default productsSlice.reducer;
