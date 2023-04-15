c50be0284ca8d931d1e252c0889ab804631e93b8
Quentin Ikeno <ikenoq@gmail.com>
2023-04-15 10:14:57 -1000

    Display products in product cards on the Store page.

--- src/pages/Store.tsx
+++ src/pages/Store.tsx
@@ -1,3 +1,33 @@
+import { useAppSelector } from "../redux/store";
+
 export default function Store() {
-	return <div>Store</div>;
+	const products = useAppSelector((state) => state.products.products);
+	const productCards = products.map((product) => (
+		<div className="column is-3" key={product.id}>
+			<div className="card ">
+				<div className="card-image">
+					<figure className="image is-128x128">
+						<img src={product.image} alt={product.title} />
+					</figure>
+				</div>
+				<div className="card-content">
+					<div className="media-content">
+						<p className="title is-4">{product.title}</p>
+						<p className="subtitle is-6">{product.price}</p>
+					</div>
+
+					<div className="content">
+						{product.rating.rate} {product.rating.count}
+					</div>
+				</div>
+			</div>
+		</div>
+	));
+	return (
+		<div>
+			<div className="columns is-desktop is-multiline">
+				{productCards}
+			</div>
+		</div>
+	);
 }
