384f7fed91cd035140b5fe669cc2cc8961e7c8c5
Quentin Ikeno <ikenoq@gmail.com>
2023-04-16 13:32:17 -1000

    Add a totalPrice and totalItems to slice and actions.

--- src/redux/cartSlice.ts
+++ src/redux/cartSlice.ts
@@ -4,13 +4,18 @@ interface CartProduct {
 	title: string;
 	image: string;
 	quantity: number;
+	price: number;
 }
 interface CartState {
 	cart: { [id: number]: CartProduct };
+	totalPrice: number;
+	totalItems: number;
 }
 
 const initialState: CartState = {
 	cart: {},
+	totalPrice: 0,
+	totalItems: 0,
 };
 
 export const cartSlice = createSlice({
@@ -24,15 +29,18 @@ export const cartSlice = createSlice({
 					id: number;
 					title: string;
 					image: string;
+					price: number;
 				};
 			}
 		) => {
-			const { id, title, image } = action.payload;
+			const { id, title, image, price } = action.payload;
 			if (Object.hasOwn(state.cart, id)) {
 				state.cart[id].quantity += 1;
 			} else {
-				state.cart[id] = { title, image, quantity: 1 };
+				state.cart[id] = { title, image, quantity: 1, price };
 			}
+			state.totalPrice += price;
+			state.totalItems += 1;
 		},
 		decrementItem: (
 			state: CartState,
@@ -41,11 +49,14 @@ export const cartSlice = createSlice({
 			}
 		) => {
 			const id = action.payload;
+			const { price } = state.cart[id];
 			if (state.cart[id].quantity === 1) {
 				delete state.cart[id];
 			} else {
 				state.cart[id].quantity -= 1;
 			}
+			state.totalPrice -= price;
+			state.totalItems -= 1;
 		},
 		deleteItem: (
 			state: CartState,
@@ -54,6 +65,9 @@ export const cartSlice = createSlice({
 			}
 		) => {
 			const id = action.payload;
+			const { price, quantity } = state.cart[id];
+			state.totalPrice -= price * quantity;
+			state.totalItems -= quantity;
 			delete state.cart[id];
 		},
 	},
