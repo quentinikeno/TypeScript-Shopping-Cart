2ebe466e49d9908cf27cb84a2d7023a0a43e9190
Quentin Ikeno <ikenoq@gmail.com>
2023-04-16 13:32:51 -1000

    Pass in price when dispatching incrementItem.

--- src/components/ProductCard.tsx
+++ src/components/ProductCard.tsx
@@ -43,7 +43,7 @@ export default function ProductCard({
 	const stars = generateStars(rate);
 
 	function handleClick() {
-		dispatch(incrementItem({ id, title, image }));
+		dispatch(incrementItem({ id, title, image, price }));
 	}
 
 	return (
