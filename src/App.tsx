203e8ec6625689dea2dbb2bcabff8770232a8f63
Quentin Ikeno <ikenoq@gmail.com>
2023-04-16 15:59:56 -1000

    Add cartSidbar component to App.

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Store from "./pages/Store";
 import Navbar from "./components/Navbar";
+import CartSidebar from "./components/cartSidebar";
 import { useAppDispatch } from "./redux/store";
 import { fetchProducts } from "./redux/productsSlice";
 
@@ -12,9 +13,10 @@ function App() {
 		dispatch(fetchProducts("https://fakestoreapi.com/products"));
 	});
 	return (
-		<div className="App ">
+		<div className="App " id="outer-container">
+			<CartSidebar />
 			<Navbar />
-			<div className="container">
+			<div className="container" id="page-wrap">
 				<Routes>
 					<Route path="/" element={<Home />} />
 					<Route path="/store" element={<Store />} />
