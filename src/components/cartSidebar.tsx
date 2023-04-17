b5c20e2313eec564ab8567273a56aa5c8dbe9714
Quentin Ikeno <ikenoq@gmail.com>
2023-04-16 15:57:36 -1000

    Create a sidebar component for shopping cart with react burger menu.

--- src/components/cartSidebar.tsx
+++ src/components/cartSidebar.tsx
@@ -1,20 +1,45 @@
-import { slide as Menu } from "react-burger-menu";
+import { slide as Menu, State } from "react-burger-menu";
+import { useAppSelector, useAppDispatch } from "../redux/store";
+import { changeOpenState } from "../redux/cartSideBarSlice";
+import "./cartSidebar.css";
 
 export default function cartSidebar() {
+	const dispatch = useAppDispatch();
+	const { isOpen } = useAppSelector((state) => state.cartSidebar);
+	const cartItems = useAppSelector((state) => state.cart);
+	const cartItemsTSX: JSX.Element[] = [];
+
+	for (const [key, value] of Object.entries(cartItems.cart)) {
+		cartItemsTSX.push(
+			<div className="box cart-item">
+				<h5 className="is-size-5 has-text-weight-bold">
+					{value.title}
+				</h5>
+				<p>
+					<i className="fa-solid fa-xmark"></i> {value.quantity}
+				</p>
+				<img src={value.image} alt={value.title} className="my-3" />
+				<p>${value.price.toFixed(2)} Each</p>
+			</div>
+		);
+	}
+
 	return (
-		<Menu>
-			<a className="menu-item" href="/">
-				Home
-			</a>
-			<a className="menu-item" href="/salads">
-				Salads
-			</a>
-			<a className="menu-item" href="/pizzas">
-				Pizzas
-			</a>
-			<a className="menu-item" href="/desserts">
-				Desserts
-			</a>
+		<Menu
+			right
+			isOpen={isOpen}
+			onStateChange={(state: State) => dispatch(changeOpenState(state))}
+			customBurgerIcon={false}
+		>
+			<h2 className="is-size-4 has-text-light">
+				Your Cart <i className="fa-solid fa-cart-shopping fa-lg"></i>
+			</h2>
+			<hr />
+			{cartItemsTSX}
+			<hr />
+			<p className="is-size-4 has-text-light pb-5">
+				Total: ${cartItems.totalPrice.toFixed(2)}
+			</p>
 		</Menu>
 	);
 }
